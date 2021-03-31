916f5aa0c767ac578e2eee7956310dcc59f4a5fa
Eragon35 <antipin35@yahoo.com>
2021-03-31 07:15:48 +0300

    Make AIinput

---
+++ src/main/scala/IO/AIInput.scala
@@ -0,0 +1,13 @@
+package IO
+
+object AIInput {
+  def input(vector: Vector[Vector[String]]): (Int, Int) = {
+    val r = scala.util.Random
+    var line = r.nextInt() % 3
+    while (!vector(line).contains(" ")) line = (line + 1) % 3
+    var row = r.nextInt() % 3
+    while (vector(line)(row) != " ") line = (line + 1) % 3
+    (line, row)
+  }
+
+}
