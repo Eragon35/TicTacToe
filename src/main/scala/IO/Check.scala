a389dbe40e1461b7b559eb45ff55da21cd008290
Eragon35 <antipin35@yahoo.com>
2021-03-31 09:52:20 +0300

    Added checker if game is over

---
+++ src/main/scala/IO/Check.scala
@@ -0,0 +1,19 @@
+package IO
+
+object Check {
+  def isFinish(vector: Vector[Vector[String]]): String = {
+    var answer = "-1"
+    vector.foreach(x => {
+      if ((x(1) == x(0)) && (x(1) == x(2))) answer = x(1)
+    })
+    for (i <- 0 to vector.size) if ((vector(1)(i) == vector(0)(i)) && (vector(1)(i) == vector(2)(i))) answer = vector(1)(i)
+    if ((vector(1)(1) == vector(0)(0)) && (vector(1)(1) == vector(2)(2))) answer = vector(1)(1)
+    if ((vector(1)(1) == vector(2)(0)) && (vector(1)(1) == vector(0)(2))) answer = vector(1)(1)
+    var full = true
+    vector.foreach(x => {
+      if (x.contains(" ")) full = false
+    })
+    if (full && (answer == "-1")) answer = "draw"
+    answer
+  }
+}
