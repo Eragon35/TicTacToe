dd238b17b58b32c0ce7d49c5ed72ef875f06159d
Eragon35 <antipin35@yahoo.com>
2021-03-31 06:35:34 +0300

    Make input coordinates

--- src/main/scala/IO/Input.scala
+++ src/main/scala/IO/Input.scala
@@ -1,8 +1,21 @@
 package IO
 
+import scala.io.StdIn
+
 object Input {
-  def readCoordinates() = {
-    
+  def readCoordinates(): (Int, Int) = {
+    print("Input coordinates: ")
+    try {
+      val input = StdIn.readLine().trim.replaceAll(",",".").split(" ")
+      val line = input(0).toInt
+      val row = input(1).toInt
+      if ((line <= 0) || (line >= 4)) throw IllegalArgumentException("Line should be between 1..3")
+      if ((row <= 0) || (row >= 4)) throw IllegalArgumentException("Row should be between 1..3")
+      (line - 1, row - 1)
+    } catch {
+      case e : Exception => Console.err.println("Reading error: " + e.getMessage)
+        readCoordinates()
+    }
   }
 
 }
